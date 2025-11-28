490357cd4e49bfb37fd39df6fb6f9ed0bffa79eb
Subhadip Adhikary <subhadip8583@gmail.com>
2025-11-28 19:30:04 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Display from './components/display.jsx'
+// import Display from './components/display.jsx'
 import Shadcn from './components/Shadcn.jsx'
 
 function App() {
